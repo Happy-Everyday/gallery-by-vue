0b6f72ef5ec06b8d714d700a6890b9379d602dca
XYTang <441089315@qq.com>
2017-12-01 17:38:45 +0800

    修改webpack配置文件

--- config/index.js
+++ config/index.js
@@ -50,12 +50,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../dist/gallery-by-vue/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../dist/gallery-by-vue'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
